8efeb34993de91cf13f99f7b818d2a4590eb9539
Giacca90 <giacca90mobile@gmail.com>
2025-08-18 21:53:39 +0200

    corregido parpadeo del modo oscuro

--- Front_Angular/src/main.ts
+++ Front_Angular/src/main.ts
@@ -1,22 +1,21 @@
-import {bootstrapApplication} from '@angular/platform-browser';
-import {AppComponent} from './app/app.component';
-import {appConfig} from './app/app.config';
+import { bootstrapApplication } from "@angular/platform-browser";
+import { AppComponent } from "./app/app.component";
+import { appConfig } from "./app/app.config";
 
 bootstrapApplication(AppComponent, appConfig).catch((err) => console.error(err));
 
-if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
-	document.body.classList.add('modo-oscuro');
-	//	document.documentElement.classList.add('modo-oscuro');
+if (window.matchMedia && window.matchMedia("(prefers-color-scheme: dark)").matches) {
+	document.body.classList.add("modo-oscuro");
 }
 
 // Slider modo oscuro en Angular
 const observer = new MutationObserver((mutations) => {
 	mutations.forEach((mutation) => {
-		if (mutation.attributeName === 'class') {
-			if (document.body.classList.contains('modo-oscuro')) {
-				document.documentElement.classList.add('modo-oscuro');
+		if (mutation.attributeName === "class") {
+			if (document.body.classList.contains("modo-oscuro")) {
+				document.documentElement.classList.add("modo-oscuro");
 			} else {
-				document.documentElement.classList.remove('modo-oscuro');
+				document.documentElement.classList.remove("modo-oscuro");
 			}
 		}
 	});
